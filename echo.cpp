bee13cde0b23e1313f40000d7ef5f12dbb9aa73f
Theeeccoo <thiagodecampos1@gmail.com>
2025-03-04 22:09:23 -0300

    [UPDATE] - Setting sockets as non_blocking ~ fcntl(2)

--- echo.cpp
+++ echo.cpp
@@ -13,7 +13,15 @@
         perror(msg);        \
         exit(EXIT_FAILURE); \
     } while (0)
+
+
 #define BUFFER_SIZE 1024
+
+struct Client
+{
+    int fd;
+};
+std::vector<Client> clients;
 
 struct sockaddr_in address;
 socklen_t address_len = sizeof(address);
@@ -23,7 +31,7 @@ int init_server_socket(void)
     int PORT = 8080;
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if ( sockfd == -1 ) handle_error("Socket Failed.");
-    // if ( fcntl(sockfd, F_SETFL, O_NONBLOCK ) ) handle_error("Failed to set socket to non_blocking.");
+    if ( fcntl(sockfd, F_SETFL, O_NONBLOCK ) ) handle_error("Failed to set socket to non_blocking.");
 
     int optval = 1;
     if ( setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval , sizeof(optval)) == - 1 ) handle_error("setsockopt Failed.");
@@ -33,42 +41,52 @@ int init_server_socket(void)
     address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
     if ( bind(sockfd, (struct sockaddr *) &address, sizeof(address)) == -1 ) handle_error("Bind Failed.");
+    if ( listen(sockfd, SOMAXCONN) == -1 ) handle_error("Listen failed.");
 
+    std::cout << "Server initiated..." << std::endl;
     return sockfd;
 }
 
 int main(void)
 {
     int sockfd = init_server_socket();
-    int clientfd;
-    char buffer[BUFFER_SIZE];
 
-    //                    4096
-    if ( listen(sockfd, SOMAXCONN) == - 1 ) handle_error("Listen Failed.");
-    if ( (clientfd = accept(sockfd, (struct sockaddr *) &address, &address_len )) == -1) handle_error("Accept Failed.");
-    std::cout << "Connected: " << clientfd << std::endl;
-
-    ssize_t amount_read    = 0;
-    ssize_t amount_written = 0;
 
     while(true)
     {
-        if ( (amount_read - amount_written) == 0 )
+        int clientfd;
+        if ( (clientfd = accept(sockfd, (struct sockaddr *) &address, &address_len )) != -1 )
         {
-            memset(&buffer, 0, BUFFER_SIZE);
-            amount_read = read(clientfd, buffer, BUFFER_SIZE);
+            clients.push_back(Client{
+                .fd = clientfd,
+            });
+            if ( fcntl(clientfd, F_SETFL, O_NONBLOCK ) ) handle_error("Failed to set client to non_blocking.");
+            std::cout << "New connection: " << clientfd << " ~ (Total: " << clients.size() << ")" << std::endl;
         }
 
-
-
-        if ( amount_read > 0 )
+        for ( size_t i = 0; i < clients.size(); i++ )
         {
-            std::cout << "SAID: " << buffer << std::endl;
-            amount_written = write(clientfd, buffer, amount_read);
+            char buffer[BUFFER_SIZE];
+            ssize_t amount_read = read(clients[i].fd, buffer, BUFFER_SIZE);
+
+            if ( amount_read > 0 )
+            {
+                // Retrieving the exact chunk of read data
+                int length = amount_read + 1
+                char chunk[length];
+                strncpy(chunk, buffer, amount_read);
+                chunk[amount_read]= '\0';
+
+                std::cout << "[" << i << "] SAID: " << chunk << std::endl;
+                ssize_t amount_written = 0;
+                while ( amount_written != length )
+                    amount_written += write(clients[i].fd, chunk, length);
+            }
         }
+
     }
 
-    close(clientfd);
+    // close(clientfd);
     close(sockfd);
     return 0;
 }
